2f481d06f29602ffb3ec96f644c55affa7f5b296
Matheus <matheusrf96@gmail.com>
2018-07-17 13:22:32 -0300

    Integração com o MySQL

--- composedjango/settings.py
+++ composedjango/settings.py
@@ -73,13 +73,23 @@ WSGI_APPLICATION = 'composedjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+} """
 
+DATABASES = {
+'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'django',
+    'USER': 'admin',
+    'PASSWORD': 'admin', 
+    'HOST': '127.0.0.1',
+    'PORT': 3306,
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
